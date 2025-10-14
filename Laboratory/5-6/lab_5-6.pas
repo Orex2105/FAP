1500d56963de499c6ccd805292292ee9768c1b14
orex2105 <pokomisa55@gmail.com>
2025-10-14 18:45:22 +0300

    update

---
+++ Laboratory/5-6/lab_5-6.pas
@@ -0,0 +1,56 @@
+﻿function removeat(var arr: array of integer; index: integer): array of integer;
+begin
+  var n := arr.high();
+  
+  for var i := index to n-1 do
+    arr[i] := arr[i + 1];
+  
+  result := arr;
+end;
+
+
+procedure one();
+begin
+  var A := arrrandom(n := 30, a := -99, b := 67);
+  var B := A.where(x -> x mod 2 = 0);
+  writeln('Массив A: ', A);
+  writeln('Массив B: ', B);
+end;
+
+
+procedure two();
+begin
+  writeln('Размер массива: ');
+  var n := readinteger();
+  writeln('Элементы массива: ');
+  var arr := readarrinteger(n);
+  println(arr.SequenceEqual(arr.sorted()));
+end;
+
+
+procedure three();
+begin
+  var arr := arrrandom(n := 20, a := -50, b := 100);
+  println('Массив: ', arr);
+  
+  var index := -1;
+  for var i := arr.High downto 0 do
+  begin
+    if arr[i] > 0 then
+    begin
+      index := i;
+      arr := removeat(arr, index);
+      SetLength(arr, arr.High);
+      break;
+    end;
+end;
+  println('Без последнего положительного элемента', arr);
+  arr := removeat(arr, arr.IndexMax());
+  SetLength(arr, arr.High);
+  print('Без наибольшего элемента', arr);
+end;
+
+
+begin
+  three();
+end.
